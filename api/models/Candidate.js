a65135e487dff72eda8a6faa531c3e971d98cbb9
Wasiq <nwasiq@gmail.com>
2019-03-24 22:12:07 +0500

    pre remove middlware for canidate added

--- api/models/Candidate.js
+++ api/models/Candidate.js
@@ -19,10 +19,10 @@ var CandidateSchema = new schema({
     }]
 });
 
-/**
- * @todo: If candidate deleted, delete all his matching criteria
- * Maybe this is already being done in delete candidate function
- */
+ CandidateSchema.pre('remove', async function() {
+     const CriteriaModel = mongoose.model("criteria");
+     await CriteriaModel.deleteMany({ candidate: this._id });
+ })
 
 const candidate = module.exports = mongoose.model('candidate', CandidateSchema);
 
